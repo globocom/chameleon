aa0f833d71494d7bd330e4b85684ccf908c1a1fe
Fabricio Lopes <fabriciolopesvital@gmail.com>
2013-09-03 15:45:01 -0300

    refatorando metodo de evento de click e seu teste

--- options.js
+++ options.js
@@ -1,13 +1,17 @@
-var group_node = document.querySelector("#group");
+(function () {
+	function addeventClick(){
+		var links_menu = document.querySelectorAll("#group_list a");
+		var group_node = document.querySelector("#group");
+	
+		group_node.textContent = localStorage['X-Mobile-Group'] || 'desktop';
 
-group_node.textContent = localStorage['X-Mobile-Group'] || 'desktop';
-
-var links_menu = document.querySelectorAll("#group_list a");
-
-for (var i=0; i < links_menu.length; i++) {
-    links_menu[i].onclick = function(){
-        var group = this.textContent;
-        localStorage['X-Mobile-Group'] = group;
-        group_node.textContent = group
-    }
-};
+		for (var i=0; i < links_menu.length; i++) {
+	    	links_menu[i].onclick = function(){
+	        	var group = this.textContent;
+	        	localStorage['X-Mobile-Group'] = group;
+	        	group_node.textContent = group
+	    	}
+		};	
+	}
+	return addeventClick();
+})();
--- test/test.options.js
+++ test/test.options.js
@@ -1,6 +1,14 @@
 describe('Options', function () {
-    it('deve trocar de device quando clicar no link', function() {
-        document.querySelector('#group').text = 'desktop';
-        expect(document.querySelector('#group').text).to.equal('desktop');
+	beforeEach(function(){
+		var link = document.querySelectorAll("#group_list a")
+    	link[0].onclick();
+	});
+
+    it('deve selecionar desktop como grupo ao clicar no link', function() {
+    	assert.equal(document.querySelector('#group').textContent, 'desktop');
+    });
+
+    it('deve setar o localStorage para desktop', function() {
+    	assert.equal(localStorage['X-Mobile-Group'], 'desktop');
     });
 });
