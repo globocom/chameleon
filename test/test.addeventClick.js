99febcc0fc61c92b565e577aa0be06c1ded84494
Fabricio Lopes <fabriciolopesvital@gmail.com>
2013-09-03 16:08:22 -0300

    colocando os icones e criando folders para organizacao

---
+++ test/test.addeventClick.js
@@ -0,0 +1,14 @@
+describe('addeventClick', function () {
+	beforeEach(function(){
+		var link = document.querySelectorAll("#group_list a")
+    	link[0].onclick();
+	});
+
+    it('deve selecionar desktop como grupo ao clicar no link', function() {
+    	assert.equal(document.querySelector('#group').textContent, 'desktop');
+    });
+
+    it('deve setar o localStorage para desktop', function() {
+    	assert.equal(localStorage['X-Mobile-Group'], 'desktop');
+    });
+});
