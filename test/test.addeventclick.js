c11a0e2a5f946708b4b5d4c7374993b8ccf263ae
Fabricio Lopes <fabriciolopesvital@gmail.com>
2013-09-04 14:33:56 -0300

    testando querySelectorAll

--- test/test.addeventclick.js
+++ test/test.addeventclick.js
@@ -1,11 +1,11 @@
 describe('addeventClick', function () {
 	beforeEach(function(){
-		var links = $("#group_list a");
-        links.eq(0).click()
+		var link = document.querySelectorAll("#group_list a")
+        link[0].onclick();
 	});
 
     it('deve selecionar desktop como grupo ao clicar no link', function() {
-        assert.equal($('#group').text(), 'desktop');
+        assert.equal(document.querySelector('#group').textContent, 'desktop');
     });
 
     it('deve setar o localStorage para desktop', function() {
