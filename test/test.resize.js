e9dbd558d11da4e0344896c84457a8a458534598
Rodrigo Lourenco <rodrigo.lourenco@corp.globo.com>
2013-09-04 14:50:02 -0300

    Alterando validação de asserts

--- test/test.resize.js
+++ test/test.resize.js
@@ -9,23 +9,21 @@ describe('Resize', function () {
     	window.open = sinon.spy();
 		response = {group: 'tablet'};
 		screenMessage(response);
-		assert.equal(window.open.getCall(0).args[2], "width=640,height=500,left=320,top=150");
+        assert.include(window.open.getCall(0).args[2], "width=640,height=500", 'incorrect values');
     });
 
     it('screenMessage deve possuir as dimensões de um smart', function(){
     	window.open = sinon.spy();
 		response = {group: 'smart'};
 		screenMessage(response);
-		assert.equal(window.open.getCall(0).args[2], "width=500,height=500,left=390,top=150");
+        assert.include(window.open.getCall(0).args[2], "width=500,height=500", 'incorrect values');
     });
 
     it('screenMessage deve possuir as dimensões de uma feature', function(){
     	window.open = sinon.spy();
 		response = {group: 'feature'};
 		screenMessage(response);
-		assert.equal(window.open.getCall(0).args[2], "width=319,height=500,left=480.5,top=150");
+        assert.include(window.open.getCall(0).args[2], "width=319,height=500", 'incorrect values');
+		
     });
-
-
-
 });
